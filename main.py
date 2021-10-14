19bfdf9b4c7d3f591ec5b026cb1b005740d8ecc1
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2021-10-14 09:03:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,13 @@ async def on_ready():
 	bot.load_extension("cogs.onMessage")
 	await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name='DMs for Help.'))
 
+@bot.command()
+async def mail(ctx,member: discord.Member = None, *,body):
+	if member == None:
+		await ctx.send("No member specified, or the member is not found!")
+	embed = discord.Embed(title = "Message from the Mods!", description = f"Hey {member.mention}!, You have received a message from the Mods @Dank Camp.")
+	embed.add_field(name = "__Author__:", value = f"{ctx.auhtor.mention}")
+	embed.add_field(name = "__Message__", value = f"{body}")
+	await member.send(embed=embed)
+
 bot.run('ODk3MzYzNTMxNjgxMzk0NzE4.YWUk6g.bvbRcMO02yioTyfn64rO7qovlUY')
