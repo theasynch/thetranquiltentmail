056425ab7d4d34084a13e67cabaafcf0381cc06c
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2022-01-19 07:40:05 +0530

    Update priv_vc.py

--- cogs/priv_vc.py
+++ cogs/priv_vc.py
@@ -6,7 +6,9 @@ import asyncio
 class PrivVC(commands.Cog):
 	def __init__(self, client):
 		self.client = client
-	
+	@commands.command()
+	async def hi(self,ctx):
+		await ctx.send('Hi')
 	@commands.Cog.listener
 	async def on_voice_state_update(self, member, before, after):
 		guild = self.client.get_guild(931771066387431505)
@@ -19,8 +21,6 @@ class PrivVC(commands.Cog):
 					member : discord.PermissionOverwrite(view_channel = True, speak = True, create_instant_invite =True)
 				}
 				priv_vc = await categ.create_voice_channel(name = f"{member.display_name}'s VC", overwrites = overwrites)
-
-
 
 
 def setup(client):
