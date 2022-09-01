29fb0c8f950dc644c6dbd5ce10daec752207d2d0
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2022-09-01 17:09:54 +0530

    Slash Commands!

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,15 +1,14 @@
 import discord
-from discord.ext import commands
 
-class Moderation(commands.Cog):
+class Moderation(discord.Cog):
+
     def __init__(self, client):
         self.client = client
 
-    @commands.slash_command(guild_ids=[862944848919003156], description="Test 1")
-    async def slash_test(self, ctx):
-        await ctx.respond("Slash COmmand Sucessful!")
+        @discord.slash_command(debug_guilds=[862944848919003156], name='bye', description='something')
+        async def goodbye(self, ctx):
+            await ctx.respond('Goodbye')
 
 
-
-def setup(client):
-	client.add_cog(Moderation(client))
+def setup(bot):
+    bot.add_cog(Moderation(bot))
--- main.py
+++ main.py
@@ -1,37 +1,23 @@
-from discord.ext import commands
 import discord
 import os
 intents = discord.Intents.default()
-# we need members intent too
 intents.members = True
+intents.message_content = True
 
-client = commands.Bot(command_prefix = "!", intents = intents)
+client = discord.Bot(debug_guilds=[862944848919003156])
 
 @client.event
 async def on_ready():
 	print("The client is online!")
 	await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name='DMs for Help.'))
 	
-
-@client.command()
-async def mail(ctx,member: discord.Member = None, *,body):
-	user = ctx.author
-	if member == None:
-		await ctx.send("No member specified, or the member is not found!")
-	embed = discord.Embed(title = "Message from the Mods!", description = f"Hey {member.mention}!, You have received a message from the Mods @Dank Camp.")
-	embed.add_field(name = "__Author__:", value = f"{user.mention}", inline = False)
-	embed.add_field(name = "__Message__", value = f"{body}", inline = False)
-	embed.set_footer(text = "Please do not reply to this message unless you want to create a thread!")
-	await member.send(embed=embed)
+@client.slash_command(name = 'test', description = "How about a test bruh?")
+async def test_it(ctx):
+	await ctx.respond("hey!")
 
 
-@client.command()
-async def chnick(ctx, member: discord.Member = None):
-	if ctx.author.id == 692295384868978710:
-		await member.edit(nick="The Tranquil Tent")
-		await ctx.send(f'Nickname was changed for {member.mention} ')
-	else:
-		pass
+
+
 
 for filename in os.listdir('./cogs'):
     # Slicing the extension for cogs.py. so that the program can understand the language.
